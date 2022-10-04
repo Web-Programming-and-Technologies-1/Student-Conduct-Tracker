391e8a4a99a72178fe6a4c719a35d918e9d5c684
JoshGopaul <josh.gopaul@my.uwi.edu>
2022-10-04 23:26:12 +0000

    manage.py

--- App/controllers/manage.py
+++ App/controllers/manage.py
@@ -1,12 +1,12 @@
-
+from App.models import  User, Review, Student
+from App.database import db
+from sqlalchemy.exc import IntegrityError
 
 @manager.command
-def make_Student(studentId, firstname, lastname, username, email ):
+def make_student():
     Joshua = Student(studentId="816", firstname="Joshua", lastname="Ali", username = "joshuaali", email="joshuaali@hotmail.com")
-    try:
+    
         db.session.add(Joshua)
         db.session.commit()
-    except IntegrityError:
-        db.session.rollback()
-        return 'ERROR: Failed to create student'
-    return 'Successfully created student'
+        print("users created")
+    

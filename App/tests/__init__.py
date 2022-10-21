55a5597cbfc60f44611a1713f16f9bee919cdd1a
tamikaramkissoon <tamika.ramkissoon@my.uwi.edu>
2022-10-21 00:45:44 +0000

    Updates model_user.py and craete  unit test for the Student model

--- App/tests/__init__.py
+++ App/tests/__init__.py
@@ -1,2 +1,3 @@
 from .test_flaskr import *
 from .model_user import *
+from .model_student import *
---
+++ App/tests/model_student.py
@@ -0,0 +1,23 @@
+import os, tempfile, pytest, logging, unittest
+# from test_flaskr import *
+from App.models import Student
+
+
+from wsgi import app
+
+
+LOGGER = logging.getLogger(__name__)
+
+'''
+   Unit 
+
+'''
+
+class StudentUnitTests(unittest.TestCase):
+
+    def test_toDict(self):
+        student = Student(studentId=1, firstname="bob", lastname="baloo", username="userbob", email="bob@gmail.com")
+        studentDict = student.toDict()
+        self.assertDictEqual(studentDict, {"id":1,"firstname": "bob", "lastname":"baloo","username":"userbob", "email":"bob@gmail.com","karmaScore":0.0})
+    
+   
